9d562879f64b98406555f0549e03aa78bedc1d1d
hutli <jensstaermose@hotmail.com>
2021-09-09 09:00:24 +0200

    Major pixel handling logic update

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,6 +1,8 @@
+use celestium::transaction::BASE_TRANSACTION_MESSAGE_LEN;
 /* Maniuplating a 1000x1000 pixel canvas */
 use rand::Rng;
 
+pub(crate) const PIXEL_HASH_SIZE: usize = 28;
 const NUM_COLORS: u8 = 16;
 const WIDTH: usize = 1000;
 const HEIGHT: usize = 1000;
@@ -9,18 +11,18 @@ pub type Color = u8;
 
 #[derive(Clone)]
 pub struct Pixel {
-    hash: [u8; 32],
+    pub hash: [u8; PIXEL_HASH_SIZE],
     pub color: Color,
 }
 
 impl Pixel {
-    pub fn new(hash: [u8; 32], color: Color) -> Pixel {
+    pub fn new(hash: [u8; PIXEL_HASH_SIZE], color: Color) -> Pixel {
         Pixel { hash, color }
     }
 
     pub fn new_rand(rng: &mut rand::rngs::ThreadRng) -> Pixel {
         Pixel {
-            hash: [0u8; 32],
+            hash: [0u8; PIXEL_HASH_SIZE],
             color: rng.gen_range(0..3),
         }
     }
@@ -33,19 +35,20 @@ pub struct Canvas {
 
 impl Canvas {
     pub fn new_test() -> Canvas {
-        let mut rng = rand::thread_rng();
+        //let mut rng = rand::thread_rng();
         let mut canvas = Vec::new();
         for _ in 0..HEIGHT {
             let mut row = Vec::new();
             for _ in 0..WIDTH {
-                row.push(Pixel::new_rand(&mut rng));
+                row.push(Pixel::new([0u8; PIXEL_HASH_SIZE], 0));
+                //row.push(Pixel::new_rand(&mut rng));
             }
             canvas.push(row);
         }
         Canvas { canvas }
     }
 
-    pub fn serialize_colors(self) -> Vec<u8> {
+    pub fn serialize_colors(&self) -> Vec<u8> {
         // serialize entire canvas into an array of only colors
         self.canvas
             .iter()
@@ -54,7 +57,7 @@ impl Canvas {
             .collect()
     }
 
-    pub fn get_pixel(self, x: usize, y: usize) -> Result<Pixel, String> {
+    pub fn get_pixel(&self, x: usize, y: usize) -> Result<Pixel, String> {
         // TODO: bounds check?
         if x < WIDTH && y < HEIGHT {
             match self.canvas.get(x) {
@@ -72,7 +75,7 @@ impl Canvas {
         }
     }
 
-    pub fn set_pixel(mut self, x: usize, y: usize, p: Pixel) -> Result<(), String> {
+    pub fn set_pixel(&mut self, x: usize, y: usize, p: Pixel) -> Result<(), String> {
         match (x < WIDTH, y < HEIGHT) {
             (true, true) => {
                 self.canvas[x as usize][y as usize] = p;
@@ -83,13 +86,17 @@ impl Canvas {
         }
     }
 
-    pub fn parse_pixel(bytes: [u8; 37]) -> Result<(usize, usize, Pixel), String> {
+    pub fn parse_pixel(
+        bytes: [u8; BASE_TRANSACTION_MESSAGE_LEN],
+    ) -> Result<(usize, usize, Pixel), String> {
         // parse the pixel data from a pixel-NFT transaction
-        let mut hash: [u8; 32] = [0u8; 32];
-        hash.copy_from_slice(&bytes[0..32]);
-        let x: usize = ((bytes[32] as usize) << 8) + (bytes[33] as usize);
-        let y: usize = ((bytes[34] as usize) << 8) + (bytes[35] as usize);
-        let color: u8 = bytes[36];
+        let mut hash: [u8; PIXEL_HASH_SIZE] = [0u8; PIXEL_HASH_SIZE];
+        hash.copy_from_slice(&bytes[..PIXEL_HASH_SIZE]);
+        let x: usize =
+            ((bytes[PIXEL_HASH_SIZE] as usize) << 8) + (bytes[PIXEL_HASH_SIZE + 1] as usize);
+        let y: usize =
+            ((bytes[PIXEL_HASH_SIZE + 2] as usize) << 8) + (bytes[PIXEL_HASH_SIZE + 3] as usize);
+        let color: u8 = bytes[PIXEL_HASH_SIZE + 4];
         match (x < WIDTH, y < HEIGHT, color < NUM_COLORS) {
             (true, true, true) => Ok((x, y, Pixel::new(hash, color))),
             (false, _, _) => Err(format!("x should be less than {}", WIDTH)),
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,60 @@
-use std::{convert::Infallible, env, fs::read, fs::File, io::prelude::*, path::PathBuf, sync::Arc};
+use std::{
+    collections::HashMap,
+    convert::Infallible,
+    env,
+    fs::read,
+    fs::File,
+    io::prelude::*,
+    path::PathBuf,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+};
 
 // external
 use cached::proc_macro::cached;
-use futures::stream::SplitSink;
-use futures::{SinkExt, StreamExt};
+use futures::{SinkExt, StreamExt, TryFutureExt};
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
-use tokio::sync::Mutex;
-use warp::{filters::ws::Message, ws::WebSocket, Filter, Rejection, Reply};
+use tokio::sync::{mpsc, Mutex, RwLock};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use warp::{filters::ws::Message, Filter, Rejection, Reply};
 
 // here
+use crate::canvas::PIXEL_HASH_SIZE;
 use celestium::{
+    merkle_forest::HASH_SIZE,
     serialize::Serialize,
-    transaction::{self, Transaction},
+    transaction::{Transaction, BASE_TRANSACTION_MESSAGE_LEN},
     wallet::{BinaryWallet, Wallet},
 };
 
 mod canvas;
 
-type SharedWallet = Arc<Mutex<Box<Wallet>>>;
-type SharedCanvas = Arc<Mutex<canvas::Canvas>>;
+////////////////////////////////////////////////// !!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+type SharedCanvas = Arc<Mutex<canvas::Canvas>>; // !!! IMPORTANT! Always lock SharedCanvas before SharedWallet  !!!
+type SharedWallet = Arc<Mutex<Box<Wallet>>>; ///// !!! IMPORTANT! to await potential dead locks (if using both) !!!
+type Clients = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Message>>>>;
+
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
 #[repr(u8)]
 #[derive(FromPrimitive)]
 enum CMDOpcodes {
-    EntireImage = 0,
-    UpdatePixel = 1,
-    Error = 2,
-    Transaction = 3,
+    Error = 0x00,
+    GetEntireImage = 0x01,
+    EntireImage = 0x02,
+    UpdatePixel = 0x03,
+    UnminedTransaction = 0x04,
+    MinedTransaction = 0x05,
+    GetPixelData = 0x06,
+    PixelData = 0x07,
+    GetStoreItems = 0x08,
+    StoreItems = 0x09,
+    BuyStoreItem = 0x0a,
+    GetUserData = 0x0b,
+    UserData = 0x0c,
 }
 
 #[tokio::main]
@@ -48,16 +75,23 @@ async fn main() {
     let shared_wallet = Arc::new(Mutex::new(Box::new(wallet)));
 
     // initialize empty canvas
-    println!("Initializing canvas...");
+    print!("Initializing canvas...");
     let canvas = canvas::Canvas::new_test();
     let shared_canvas = Arc::new(Mutex::new(canvas));
-    println!("Canvas initialized.");
+    println!(" Done!");
+
+    // Keep track of all connected users, key is usize, value
+    // is a websocket sender.
+    let clients = Clients::default();
+    // Turn our "state" into a new Filter...
+    let clients = warp::any().map(move || clients.clone());
 
     // configure ws route
     let ws_route = warp::path::end()
         .and(warp::ws())
-        .and(with_wallet(shared_wallet.clone()))
-        .and(with_canvas(shared_canvas.clone()))
+        .and(with_wallet(shared_wallet))
+        .and(with_canvas(shared_canvas))
+        .and(clients)
         .and_then(ws_handler)
         .with(warp::cors().allow_any_origin());
 
@@ -158,11 +192,12 @@ async fn ws_handler(
     ws: warp::ws::Ws,
     wallet: SharedWallet,
     canvas: SharedCanvas,
+    clients: Clients,
 ) -> Result<impl Reply, Rejection> {
     // weird boilerplate because I don't know why
     // this async function seems to just pass stuff on to another async function
     // but I don't know how to inline it 🤷
-    Ok(ws.on_upgrade(move |socket| client_connection(socket, wallet, canvas)))
+    Ok(ws.on_upgrade(move |socket| client_connection(socket, wallet, canvas, clients)))
 }
 
 macro_rules! ws_error {
@@ -170,26 +205,50 @@ macro_rules! ws_error {
     // and exit function early
     ($sender: expr, $errmsg: expr) => {
         println!("{}", $errmsg);
-        $sender.send(Message::text($errmsg)).await.unwrap();
+        $sender.send(Message::text($errmsg)).unwrap();
         return;
     };
 }
 
-async fn client_connection(ws: warp::ws::WebSocket, wallet: SharedWallet, canvas: SharedCanvas) {
+macro_rules! unwrap_or_ws_error {
+    ($sender: expr, $result: expr) => {
+        match $result {
+            Ok(r) => r,
+            Err(e) => {
+                ws_error!($sender, e);
+            }
+        }
+    };
+}
+
+async fn client_connection(
+    ws: warp::ws::WebSocket,
+    wallet: SharedWallet,
+    canvas: SharedCanvas,
+    clients: Clients,
+) {
     // keeps a client connection open, pass along incoming messages
     println!("Establishing client connection... {:?}", ws);
     let (mut sender, mut receiver) = ws.split();
-    {
-        // get current canvas, send it to the new client
-        let real_canvas = canvas.lock().await;
-        let mut initial_pic = real_canvas.to_owned().serialize_colors();
-        initial_pic.insert(0, CMDOpcodes::EntireImage as u8);
-        drop(real_canvas);
-        // canvas released
-        if let Err(e) = sender.send(Message::binary(initial_pic)).await {
-            ws_error!(sender, format!("Error sending canvas: {}", e));
+
+    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
+
+    let (tx, rx) = mpsc::unbounded_channel();
+    let mut rx = UnboundedReceiverStream::new(rx);
+
+    tokio::task::spawn(async move {
+        while let Some(message) = rx.next().await {
+            sender
+                .send(message)
+                .unwrap_or_else(|e| {
+                    println!("websocket send error: {}", e);
+                })
+                .await;
         }
-    }
+    });
+
+    // Save the sender in our list of connected users.
+    clients.write().await.insert(my_id, tx);
 
     while let Some(body) = receiver.next().await {
         let message = match body {
@@ -199,32 +258,45 @@ async fn client_connection(ws: warp::ws::WebSocket, wallet: SharedWallet, canvas
                 break;
             }
         };
-        handle_ws_message(message, &mut sender, wallet.clone(), canvas.clone()).await;
+        handle_ws_message(message, my_id, wallet.clone(), canvas.clone(), &clients).await;
     }
-}
 
-async fn ws_error(errmsg: String, sender: &mut SplitSink<WebSocket, Message>) {
-    println!("{}", errmsg);
-    sender.send(Message::text(errmsg)).await.unwrap();
+    clients.write().await.remove(&my_id);
 }
 
 async fn handle_ws_message(
     message: Message,
-    sender: &mut SplitSink<WebSocket, Message>,
+    my_id: usize,
     wallet: SharedWallet,
     canvas: SharedCanvas,
+    clients: &Clients,
 ) {
     // this is the function that actually receives a message
     // validate it, add it to the blockchain, then exit.
 
+    let real_clients = clients.read().await;
+    let sender = real_clients.get(&my_id).unwrap();
+
     if !message.is_binary() {
-        ws_error!(sender, "Error: expected binary WS message.".to_string());
+        ws_error!(sender, "Expected binary WS message.".to_string());
     }
 
     let binary_message = message.as_bytes();
     match FromPrimitive::from_u8(binary_message[0]) {
-        Some(CMDOpcodes::Transaction) => {
-            parse_transaction(&binary_message[1..], sender, wallet, canvas)
+        Some(CMDOpcodes::GetEntireImage) => {
+            let real_canvas = canvas.lock().await;
+            let mut entire_image = real_canvas.serialize_colors();
+            entire_image.insert(0, CMDOpcodes::EntireImage as u8);
+            drop(real_canvas);
+            if let Err(e) = sender.send(Message::binary(entire_image)) {
+                ws_error!(sender, format!("Error sending canvas: {}", e));
+            }
+        }
+        Some(CMDOpcodes::MinedTransaction) => {
+            parse_transaction(&binary_message[1..], sender, wallet, canvas, clients).await
+        }
+        Some(CMDOpcodes::GetPixelData) => {
+            parse_get_pixel_data(&binary_message[1..], sender, wallet, canvas).await
         }
         _ => {
             ws_error!(
@@ -233,68 +305,102 @@ async fn handle_ws_message(
             );
         }
     }
-    .await;
+}
+
+async fn parse_get_pixel_data(
+    bin_parameters: &[u8],
+    sender: &mpsc::UnboundedSender<Message>,
+    wallet: SharedWallet,
+    canvas: SharedCanvas,
+) {
+    if bin_parameters.len() != 4 {
+        ws_error!(
+            sender,
+            format!(
+                "Expected message len of {} for CMD Opcode {:x} (Get pixel hash) got {}",
+                5,
+                CMDOpcodes::GetPixelData as u8,
+                bin_parameters.len()
+            )
+        );
+    }
+    let mut pixel_hash_response = [0u8; 1 + PIXEL_HASH_SIZE + HASH_SIZE];
+    pixel_hash_response[0] = CMDOpcodes::PixelData as u8;
+    {
+        let real_canvas = canvas.lock().await;
+        let x: usize = ((bin_parameters[0] as usize) << 8) + (bin_parameters[1] as usize);
+        let y: usize = ((bin_parameters[2] as usize) << 8) + (bin_parameters[3] as usize);
+        let pixel = unwrap_or_ws_error!(sender, real_canvas.get_pixel(x, y));
+        pixel_hash_response[1..PIXEL_HASH_SIZE + 1].copy_from_slice(&pixel.hash);
+        drop(real_canvas);
+    }
+    {
+        let real_wallet = wallet.lock().await;
+        let block_hash = real_wallet.get_head_hash();
+        pixel_hash_response[PIXEL_HASH_SIZE + 1..].copy_from_slice(&block_hash);
+        drop(real_wallet);
+    }
+
+    if let Err(e) = sender.send(Message::binary(pixel_hash_response)) {
+        println!("Could not send pixel hash: {}", e);
+    };
 }
 
 async fn parse_transaction(
     bin_transaction: &[u8],
-    sender: &mut SplitSink<WebSocket, Message>,
+    sender: &mpsc::UnboundedSender<Message>,
     wallet: SharedWallet,
     canvas: SharedCanvas,
+    clients: &Clients,
 ) {
-    // parse binary transaction
-    let transaction = match Transaction::from_serialized(bin_transaction, &mut 0) {
-        Ok(transaction) => transaction,
-        Err(e) => {
-            ws_error!(sender, format!("Error: Could not parse transaction: {}", e));
-        }
-    };
+    let transaction = unwrap_or_ws_error!(
+        sender,
+        Transaction::from_serialized(bin_transaction, &mut 0)
+    );
 
-    {
-        // transaction parses! get the shared wallet.
-        let mut real_wallet = wallet.lock().await;
+    let mut real_wallet = wallet.lock().await;
+    if let Err(e) = real_wallet.add_off_chain_transaction(*transaction.clone()) {
+        ws_error!(sender, e);
+    }
 
-        // add transaction to queue
-        if let Err(e) = real_wallet.add_off_chain_transaction(*transaction.clone()) {
-            ws_error(format!("Error: Could not add transaction: {}", e), sender).await;
-        }
+    if let Err(e) = save_wallet(&real_wallet) {
+        ws_error!(sender, e);
+    }
+    drop(real_wallet);
 
-        // store new wallet
-        if let Err(e) = save_wallet(&real_wallet) {
-            ws_error(
-                format!("Error: valid transaction couldn't be stored: {}", e),
-                sender,
+    let base_transaction_message =
+        unwrap_or_ws_error!(sender, transaction.get_base_transaction_message())
+            as [u8; BASE_TRANSACTION_MESSAGE_LEN];
+
+    let mut real_canvas = canvas.lock().await;
+    let (x, y, pixel) = unwrap_or_ws_error!(
+        sender,
+        canvas::Canvas::parse_pixel(base_transaction_message)
+    );
+    let current_pixel_hash = unwrap_or_ws_error!(sender, real_canvas.get_pixel(x, y))
+        .hash
+        .to_vec();
+    let new_pixel_back_ref_hash = base_transaction_message[..PIXEL_HASH_SIZE].to_vec();
+    if current_pixel_hash != new_pixel_back_ref_hash {
+        ws_error!(
+            sender,
+            format!(
+                "Got wrong pixel hash expected {:x?} got {:x?}",
+                current_pixel_hash, new_pixel_back_ref_hash,
             )
-            .await;
+        );
+    } else {
+        unwrap_or_ws_error!(sender, real_canvas.set_pixel(x, y, pixel));
+        let mut update_pixel_binary_message = [0u8; 6];
+        update_pixel_binary_message[0] = CMDOpcodes::UpdatePixel as u8;
+        update_pixel_binary_message[1..]
+            .copy_from_slice(&base_transaction_message[PIXEL_HASH_SIZE..]);
+        println!("Len: {}", clients.read().await.len());
+        for tx in clients.read().await.values() {
+            if let Err(e) = tx.send(Message::binary(update_pixel_binary_message)) {
+                println!("Could not send updated pixel: {}", e);
+            }
         }
-
-        drop(real_wallet)
-    } // mutex lock released
-
-    // if the transaction has a base message, it's likely a pixel NFT
-    // XXX: this will change later, when we add voting NFTs
-    if let Ok(base_transaction_message) = transaction.get_base_transaction_message() {
-        // get canvas and add the new transaction
-        let real_canvas = canvas.lock().await;
-        if let Err(e) = transaction_to_canvas(real_canvas.to_owned(), base_transaction_message) {
-            ws_error!(sender, format!("Error: {}", e));
-        }
-        drop(real_canvas);
-    } // shared canvas released
-
-    // TODO inform all the other ws clients about the new pixel
-}
-
-fn transaction_to_canvas(
-    canvas: canvas::Canvas,
-    base_transaction_message: [u8; transaction::BASE_TRANSACTION_MESSAGE_LEN],
-) -> Result<(), String> {
-    // extract and parse pixel data
-    let mut pixeldata = [0u8; 37];
-    pixeldata.copy_from_slice(&base_transaction_message[0..38]);
-    let (x, y, pixel): (usize, usize, canvas::Pixel) = canvas::Canvas::parse_pixel(pixeldata)?;
-
-    // add it to the canvas
-    canvas.set_pixel(x, y, pixel)?;
-    Ok(())
+    }
+    drop(real_canvas);
 }
