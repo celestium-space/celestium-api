e5ddd518e88daf2805a1f4f144738b1a34b2c969
hutli <jensstaermose@hotmail.com>
2022-02-09 18:56:38 +0100

    Working example python script

--- example-pixel-script.py
+++ example-pixel-script.py
@@ -1,19 +1,38 @@
-# std lib
 import asyncio
-from enum import Enum
 import itertools
 import math
 import struct
-from hashlib import sha3_256
-from multiprocessing import Pool
+import time
+from enum import Enum
 from functools import partial
+from hashlib import sha3_256
+from multiprocessing import Pool, cpu_count
 from typing import Optional
+from tqdm import tqdm
+from logo import logo
 
-# 3d party
+import ecdsa
 import websockets
 
-# Get pubkey from js console: localStorage.getItem("pk_bin")
-pubkey = bytes.fromhex("03c99405208391fdb33ce374ff7fbda265f8188dea3d4a4dcdb7bb01f7d36c024b")
+color_map = [
+    0x000000,
+    0xE50000,
+    0x02BE01,
+    0x0000EA,
+    0xF8F208,
+    0xFD5EF8,
+    0x00D3DD,
+    0xFFFFFF,
+    0x7415CD,
+    0xF3C99D,
+    0x999999,
+    0xE59500,
+    0x0083C7,
+    0x347115,
+    0x43270A,
+    0x865A48,
+]
+
 
 class Ops(Enum):
     get_canvas = 0x01
@@ -26,139 +45,171 @@ class Ops(Enum):
 
 
 def magic_bytes(*args, **kwargs):
-    """ Make magic bytes for mining.
-        Arguments are passed to `itertools.count`
+    """Make magic bytes for mining.
+    Arguments are passed to `itertools.count`
     """
     for i in itertools.count(*args, **kwargs):
         # find out how many bytes to make for `i`
-        num_bytes = math.ceil(math.ceil(math.log2(i+1)) / 8) or 1
+        num_bytes = math.ceil(math.ceil(math.log2(i + 1)) / 8) or 1
 
         # make bytes from `i`
-        i_bytes = i.to_bytes(num_bytes, 'big')
+        i_bytes = i.to_bytes(num_bytes, "big")
         assert len(i_bytes) == num_bytes and len(i_bytes) > 0
 
         # add continuation bits for every byte except last
-        bs = bytes([b | 0x80 for b in i_bytes[:-1]]) + bytes([i_bytes[-1] & 0x7f])
+        bs = bytes([b | 0x80 for b in i_bytes[:-1]]) + bytes([i_bytes[-1] & 0x7F])
         assert len(bs) == num_bytes
 
         # yield magic bytes value
         yield bs
 
 
-def miner(transaction_hash: bytes, thread_count: int, thread_num: int) -> Optional[bytes]:
+def miner(
+    transaction_hash: bytes, thread_count: int, thread_num: int
+) -> Optional[bytes]:
     for mb in magic_bytes(start=thread_num, step=thread_count):
         if sha3_256(transaction_hash + mb).digest().startswith(b"\0\0\0"):
-            print("Mining complete!")
             return mb
     return None
 
 
 async def main():
-    async with websockets.connect("wss://api.celestium.space/", ping_interval=None) as ws:
+    async with websockets.connect(
+        "wss://api.celestium.space/", ping_interval=None
+    ) as ws:
+        # Get PK from exported SK
+        with open("sk.txt") as f:
+            pk = (
+                ecdsa.SigningKey.from_string(
+                    bytes.fromhex(f.read()), curve=ecdsa.SECP256k1
+                )
+                .get_verifying_key()
+                .to_string("compressed")
+            )
 
-        # get canvas
-        await ws.send(bytes([Ops.get_canvas.value]))
-        r = await ws.recv()
-        assert r[0] == Ops.got_canvas.value
-        assert len(r) == 1_000_001
-        # print(f"Got something: {len(r)} bytes")
-        print(f"Canvas: {r[:50]}")
+        pixels = []
+        x_offset = 50
+        y_offset = 50
+        text_color = 4
+        background_color = 3
 
-        # mine a pixel
-        px, py, c = 0, 0, 14
+        for py, row in enumerate(logo):
+            for px, color in enumerate(row):
+                pixels.append(
+                    (
+                        px + x_offset,
+                        py + y_offset,
+                        text_color if color else background_color,
+                    )
+                )
 
-        # get previous pixel hash
-        req = bytes([Ops.get_pixel.value]) + px.to_bytes(2, "big") + py.to_bytes(2, "big") + pubkey
-        print(f"Sending request for pixel hash: {req=}")
-        await ws.send(req)
+        start_time = time.time()
+        for px, py, c in tqdm(pixels):
+            # get previous pixel hash
+            req = (
+                bytes([Ops.get_pixel.value])
+                + px.to_bytes(2, "big")
+                + py.to_bytes(2, "big")
+                + pk
+            )
+            # Sending request for pixel hash: {req=}")
+            await ws.send(req)
 
-        print("Waiting for pixel hash response...")
-        while True:
-            r = await ws.recv()
-            if isinstance(r, str):
-                print("Error: " + r)
-                quit(1)
-            if r.startswith(bytes([Ops.got_pixel.value])):
-                print("Got pixel response")
-                break
-            print(f"Ignoring opcode: {r[0]}")
+            # Waiting for pixel hash response...
+            while True:
+                r = await ws.recv()
+                if isinstance(r, str):
+                    print("Error: " + r)
+                    quit(1)
+                if r.startswith(bytes([Ops.got_pixel.value])):
+                    break
 
-        # parse previous pixel hash stuff
-        print(f"Pixel len is {len(r)}")
-        assert r[0] == Ops.got_pixel.value, f"Unexpected opcode: {r[0:50]}"
+            # parse previous pixel hash stuff
+            assert r[0] == Ops.got_pixel.value, f"Unexpected opcode: {r[0:50]}"
 
-        prev_pixel_hash = r[1:29]
-        print(f"{len(prev_pixel_hash)=}")
-        assert len(prev_pixel_hash) == 28
+            prev_pixel_hash = r[1:29]
+            assert len(prev_pixel_hash) == 28
 
-        block_head_hash = r[29:61]
-        print(f"{len(block_head_hash)=}")
-        assert len(block_head_hash) == 32
+            block_head_hash = r[29:61]
+            assert len(block_head_hash) == 32
 
-        katjing_transaction = r[61:]
-        print(len(katjing_transaction))
-        print(f"{len(katjing_transaction)=}")
+            katjing_transaction = r[61:-1]
 
-        # build a pixel NFT transaction
-        transaction_len = sum([
-            version_len := 1,
-            input_count_len := 1,
-            input_blockhash_len := 32,
-            input_message_len := 32,
-            input_index_len := 1,
-            output_count_len := 1,
-            output_value_version_len := 1,
-            output_value_id_len := 32,
-            output_pk_len := 33,
-        ])
-        transaction: bytes = b"".join([
-            version         := bytes([0]),
-            input_count     := bytes([0]),
-            block_head_hash, # already defined
-            prev_pixel_hash, # already defined
-            x_16b           := px.to_bytes(2, 'big'),
-            y_16b           := py.to_bytes(2, 'big'),
-            c_8b            := bytes([c]),
-            output_count    := bytes([1]),
-            output_value_id := bytes([1]),
-            nft_hash        := sha3_256(prev_pixel_hash
-                                        + x_16b + y_16b + c_8b
-                                        + output_count).digest(),
-            pubkey           # already defined
-        ])
-        print(f"{len(transaction)=}")
-        assert transaction_len == len(transaction)
+            # build a pixel NFT transaction
+            transaction_len = sum(
+                [
+                    version_len := 1,
+                    input_count_len := 1,
+                    input_blockhash_len := 32,
+                    input_message_len := 32,
+                    input_index_len := 1,
+                    output_count_len := 1,
+                    output_value_version_len := 1,
+                    output_value_id_len := 32,
+                    output_pk_len := 33,
+                ]
+            )
+            pixel_transaction: bytes = b"".join(
+                [
+                    version := bytes([0]),
+                    input_count := bytes([0]),
+                    block_head_hash,  # already defined
+                    prev_pixel_hash,  # already defined
+                    x_16b := px.to_bytes(2, "big"),
+                    y_16b := py.to_bytes(2, "big"),
+                    c_8b := bytes([c]),
+                    output_count := bytes([1]),
+                    output_value_id := bytes([1]),
+                    nft_hash := sha3_256(
+                        prev_pixel_hash + x_16b + y_16b + c_8b
+                    ).digest(),
+                    pk,  # already defined
+                ]
+            )
 
-        # mine transaction in parallel
-        thread_count = 8
-        p = Pool(thread_count)
-        print(f"Mining with {thread_count=}...")
-        mb = next(p.imap_unordered(
-            partial(miner, sha3_256(transaction).digest(), thread_count),
-            range(thread_count)
-        ))
-        p.terminate()
-        print(f"Found {mb=}")
+            assert transaction_len == len(pixel_transaction)
 
-        # send mined transaction back to api
-        mined_transaction = transaction + mb
+            # Mine pixel transaction in parallel
+            thread_count = cpu_count()
+            p = Pool(thread_count)
+            mb = next(
+                p.imap_unordered(
+                    partial(miner, sha3_256(pixel_transaction).digest(), thread_count),
+                    range(thread_count),
+                )
+            )
+            p.terminate()
+            pixel_transaction = pixel_transaction + mb
 
-        print(f"Sending mined transaction")
-        await ws.send(bs := bytes([Ops.mined_transaction.value, 0x01]) + mined_transaction)
+            # send mined transaction back to api
+            await ws.send(
+                bs := bytes([Ops.mined_transaction.value]) + pixel_transaction
+            )
 
-        print(f"{mined_transaction=}")
+            # Mine katjing transaction in parallel
+            thread_count = cpu_count()
+            p = Pool(thread_count)
+            mb = next(
+                p.imap_unordered(
+                    partial(
+                        miner, sha3_256(katjing_transaction).digest(), thread_count
+                    ),
+                    range(thread_count),
+                )
+            )
+            p.terminate()
+            katjing_transaction = katjing_transaction + mb
 
-        print(f"total payload: {bs}")
+            # send mined transaction back to api
+            await ws.send(
+                bs := bytes([Ops.mined_transaction.value]) + katjing_transaction
+            )
 
-        print(f"Mined transaction sent! Waiting for error or other response...")
-        while r := await ws.recv():
-            if isinstance(r, str):
-                print(f"Error: {r=}")
-            elif r[0] == Ops.update_pixel.value:
-                print(f"Skipping opcode: {r[0]=}")
-            else:
-                print(f"Got opcode: {r[0]=}")
-                break
+        took = time.time() - start_time
+        print(
+            f"Avg mining time: {took/(len(pixels)*2)}s ({took/cpu_count()/(len(pixels)*2)}s per core)"
+        )
 
 
 asyncio.run(main())
+
